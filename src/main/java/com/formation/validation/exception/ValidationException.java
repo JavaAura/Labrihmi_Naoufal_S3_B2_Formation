e6cf513f18a56bf0e0a778cd85aa7b49bebcfa36
Naoufal <naoufal.labrihmi@gmail.com>
2024-10-30 12:08:52 +0100

    Create ValidationException.java

---
+++ src/main/java/com/formation/validation/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.formation.validation.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
